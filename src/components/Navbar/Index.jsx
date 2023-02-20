b2df6f9fc953ae82f1f19851fe7cb359ffb80953
FedeSabi <fsabiche@yahoo.com>
2023-02-19 22:24:30 -0300

    navbar

--- src/components/Navbar/Index.jsx
+++ src/components/Navbar/Index.jsx
@@ -1,13 +1,15 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './Navbar.css'
 
 const Navbar = () => {
+   
     return(
         <div>
             <header>
         <h1 className="titulo">Federico</h1>
+       
         <nav>
-            <div className="menu">
+           <div className="menu">
             <a href="#sobreMi">Sobre Mi</a>
             <a href="#proyectos">Proyectos</a>
             <a href="#contacto">Contacto</a>
